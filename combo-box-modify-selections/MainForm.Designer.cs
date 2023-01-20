279a4e1aa63b9e266a7a89fc9791637a0a2430ba
Tom Gregor <tgregor@ivsoftware.com>
2023-01-20 11:07:16 -0700

    Add project files.

---
+++ combo-box-modify-selections/MainForm.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace combo_box_modify_selections
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBoxCom = new System.Windows.Forms.ComboBox();
+            this.richTextBox = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // comboBoxCom
+            // 
+            this.comboBoxCom.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.comboBoxCom.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.comboBoxCom.FormattingEnabled = true;
+            this.comboBoxCom.Location = new System.Drawing.Point(37, 394);
+            this.comboBoxCom.Name = "comboBoxCom";
+            this.comboBoxCom.Size = new System.Drawing.Size(178, 40);
+            this.comboBoxCom.TabIndex = 0;
+            // 
+            // richTextBox
+            // 
+            this.richTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox.Location = new System.Drawing.Point(37, 21);
+            this.richTextBox.Name = "richTextBox";
+            this.richTextBox.Size = new System.Drawing.Size(633, 367);
+            this.richTextBox.TabIndex = 1;
+            this.richTextBox.Text = "";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(728, 444);
+            this.Controls.Add(this.richTextBox);
+            this.Controls.Add(this.comboBoxCom);
+            this.Name = "MainForm";
+            this.Text = "Main Form";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ComboBox comboBoxCom;
+        private RichTextBox richTextBox;
+    }
+}
---
+++ combo-box-modify-selections/MainForm.cs
@@ -0,0 +1,140 @@
+using Microsoft.Win32;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO.Ports;
+using System.Management;
+
+namespace combo_box_modify_selections
+{
+    public partial class MainForm : Form
+    {
+        public MainForm() => InitializeComponent();
+        const int WM_DEVICECHANGE = 0x0219;
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+            if(m.Msg == WM_DEVICECHANGE) 
+            {
+                onDeviceChange();
+            }
+        }
+        BindingList<ComPort> ComPorts = new BindingList<ComPort>();
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            comboBoxCom.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBoxCom.DropDownClosed += (sender, e) => ActiveControl = null;
+            comboBoxCom.DataSource = ComPorts;
+            ComPorts.ListChanged += onComPortChanged;
+            enumerateComPorts();
+            _initialized= true;
+        }
+        bool _initialized = false;
+
+        private void onComPortChanged(object? sender, ListChangedEventArgs e)
+        {
+            if(_initialized) BeginInvoke(() => richTextBox.Clear());
+            ComPort comPort;
+            switch (e.ListChangedType)
+            {
+                case ListChangedType.ItemAdded:
+                    comPort = ComPorts[e.NewIndex];
+                    using (ManagementClass manager = new ManagementClass("Win32_PnPEntity"))
+                    {
+                        foreach (ManagementObject record in manager.GetInstances())
+                        {
+                            var pnpDeviceId = record.GetPropertyValue("PnpDeviceID")?.ToString();
+                            if (pnpDeviceId != null)
+                            {
+                                var subkey = Path.Combine("System", "CurrentControlSet", "Enum", pnpDeviceId, "Device Parameters");
+                                var regKey = Registry.LocalMachine.OpenSubKey(subkey);
+                                if (regKey != null)
+                                {
+                                    var names = regKey.GetValueNames();
+                                    if (names.Contains("PortName"))
+                                    {
+                                        var portName = regKey.GetValue("PortName");
+                                        if (Equals(comPort.PortName, portName))
+                                        {
+                                            var subkeyParent = Path.Combine("System", "CurrentControlSet", "Enum", pnpDeviceId);
+                                            var regKeyParent = Registry.LocalMachine.OpenSubKey(subkeyParent);
+                                            comPort.FriendlyName = $"{regKeyParent?.GetValue("FriendlyName")}";
+                                            comPort.PnpDeviceId = pnpDeviceId;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    break;
+                case ListChangedType.ItemDeleted:
+                    comPort = _removedItem!;
+                    break;
+                default: return;
+            }
+            BeginInvoke(() =>
+            {
+                richTextBox.SelectionColor = Color.Red;
+                richTextBox.AppendText($"{e.ListChangedType}{Environment.NewLine}");
+                richTextBox.SelectionColor = Color.FromArgb(0x20, 0x20, 0x20);
+                richTextBox.AppendText(comPort?.GetFullDescription());
+            });
+        }
+
+        int _wdtCount = 0;
+        private ComPort? _removedItem = null;
+
+        private void onDeviceChange()
+        {
+            int captureCount = ++_wdtCount;
+            Task
+                .Delay(TimeSpan.FromMilliseconds(500))
+                .GetAwaiter()
+                .OnCompleted(() =>
+                {
+                    if(captureCount.Equals(_wdtCount))
+                    {
+                        // The events have settled out
+                        enumerateComPorts();
+                    }
+                });
+        }
+        private void enumerateComPorts()
+        {
+            string[] ports = SerialPort.GetPortNames();
+            foreach (var port in ports)
+            {
+                if (!ComPorts.Any(_ => Equals(_.PortName, port)))
+                {
+                    ComPorts.Add(new ComPort { PortName = port });
+                }
+            }
+            foreach (var port in ComPorts.ToArray())
+            {
+                if (!ports.Contains(port.PortName))
+                {
+                    ComPorts.Remove(port);
+                    _removedItem = port;
+                }
+            }
+            BeginInvoke(()=> ActiveControl = null); // Remove focus rectangle
+        }
+    }
+    class ComPort
+    {
+        public string? PortName { get; set; }
+        public string? FriendlyName { get; set; }
+        public string? PnpDeviceId { get; set; }
+        public override string ToString() => PortName ?? "None";
+        public string GetFullDescription()
+        {
+            var builder = new List<string>();
+            builder.Add($"{PortName}");
+            builder.Add($"{FriendlyName}");
+            builder.Add($"{nameof(PnpDeviceId)}: ");
+            builder.Add($"{PnpDeviceId}");
+            return $"{string.Join(Environment.NewLine, builder)}{Environment.NewLine}{Environment.NewLine}";
+        }
+    }
+}
